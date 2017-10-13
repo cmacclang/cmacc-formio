982359c1d9a232f68542de242a384493cc3a1448
Vincent de Bruijn <bruijnv@gmail.com>
2017-10-13 16:55:59 +0200

    submit data to endpoint

--- src/app/views/Home.js
+++ src/app/views/Home.js
@@ -3,6 +3,10 @@ import {PropTypes} from 'prop-types';
 import {Formio} from 'react-formio';
 import FormioView from 'react-formio/lib/FormioView';
 //import Hero from '../containers/Hero';
+/* eslint no-console: 0*/
+
+const form = 'https://vsoqkxzelfinxcb.form.io/cmacc-escrow';
+const endpoint = 'https://cmacc.herokuapp.com/formio/cmacc-escrow/';
 
 export default class HomeView extends FormioView {
   component = class Home extends Component {
@@ -10,11 +14,16 @@ export default class HomeView extends FormioView {
       auth: PropTypes.object.isRequired
     };
 
+    handleSubmit = (event) => {
+      window.location.href = `${endpoint}${event._id}`;
+      return {};
+    }
+
     render() {
       const {auth} = this.props;
       return (
         <div>
-          <Formio src="https://rytgokqjbsjtuez.form.io/introductie" />
+          <Formio src={form} onSubmit={this.handleSubmit} />
         </div>
       );
     }
